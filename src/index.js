ed68e715208e103564909ae18c644ad810398def
hyunkim <hyunkim@gmail.com>
2021-03-25 13:42:35 +0900

    #Recap Refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
+import {createStore} from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updatenumber = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case "ADD" :
+      return count + 1;
+    case "MINUS" :
+      return count - 1;
+    default:
+      return count;
+  }
 }
 
-const plusEvent = () =>{
-  count += 1;
-  updatenumber();
+const countStore = createStore(countModifier);
+
+const countUpdate = () => {
+  number.innerText = countStore.getState();
 }
 
-const minusEvent = () =>{
-  count -= 1;
-    updatenumber();
+countStore.subscribe(countUpdate);
+
+const handlePlus = () =>{
+  countStore.dispatch({type:"ADD"});
 }
 
-plus.addEventListener("click", plusEvent);
-minus.addEventListener("click", minusEvent);
+const handleMinus = () =>{
+  countStore.dispatch({type:"MINUS"});
+}
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
+
